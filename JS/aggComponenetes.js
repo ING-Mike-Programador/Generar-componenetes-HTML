2c5b3abc31e9a130dfd9f904945f734ffd886bf7
ING-Mike-Programador <miguel_alonso44@hotmail.com>
2025-02-23 16:46:07 -0700

    Comienzo JS

---
+++ JS/aggComponenetes.js
@@ -0,0 +1,6 @@
+function crearComponente(){
+    $div = document.getElementById("AgregarComponenetes");
+    $div2 = document.getElementById("AgregarComponenetes").getHTML();
+    //window.alert("Primero: \n"+$div);
+    window.alert("Segundo: \n"+$div2);
+}
